d33a933f04179ad818592e8df6a0c38080ce4964
tjc-code <thomasjclarke.work@gmail.com>
2024-04-04 13:34:16 +0000

    Fix bug where people with some ? were removed

--- cleaner.py
+++ cleaner.py
@@ -10,6 +10,13 @@ config = vars(args)
 
 df = pd.read_excel(f"{config[file_name]}.xlsx")
 # filter out "?" availabilities
-filtered_df = df[~(df.iloc[:, 1:] == "? ").any(axis=1)].rename(columns={"Unnamed: 0": "name"})
-print(filtered_df)
+filtered_df = df[
+    ~(
+        df.iloc[:, 1:]
+        .astype(str)
+        .apply(lambda x: x.str.contains(r"\?", regex=True))
+        .all(axis=1)
+    )
+].rename(columns={"Unnamed: 0": "name"})
+
 filtered_df.to_excel(f"cleaner_{config[file_name]}.xlsx", index=False)
