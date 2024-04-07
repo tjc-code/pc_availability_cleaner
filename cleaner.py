abb3ddd19df6db89353aedc0ed8dc62d2417f67e
tjc-code <thomasjclarke.work@gmail.com>
2024-04-07 13:24:59 +0000

    Modify for running as Dash app

--- cleaner.py
+++ cleaner.py
@@ -1,22 +1,29 @@
 import pandas as pd
 import argparse
 
-# parse command line argument of the file name
-parser = argparse.ArgumentParser()
-file_name = "file_name"
-parser.add_argument(file_name, help="The file you want to clean without extension")
-args = parser.parse_args()
-config = vars(args)
+def clean_dataframe(df: pd.DataFrame, file_name):
+    # filter out "?" availabilities
+    filtered_df = df[
+        ~(
+            df.iloc[:, 1:]
+            .astype(str)
+            .apply(lambda x: x.str.contains(r"\?", regex=True))
+            .all(axis=1)
+        )
+    ].rename(columns={"Unnamed: 0": "name"})
 
-df = pd.read_excel(f"{config[file_name]}.xlsx")
-# filter out "?" availabilities
-filtered_df = df[
-    ~(
-        df.iloc[:, 1:]
-        .astype(str)
-        .apply(lambda x: x.str.contains(r"\?", regex=True))
-        .all(axis=1)
-    )
-].rename(columns={"Unnamed: 0": "name"})
+    # filtered_df.to_excel(f"cleaner_{file_name}", index=False)
+    return filtered_df
 
-filtered_df.to_excel(f"cleaner_{config[file_name]}.xlsx", index=False)
+
+if __name__ == "__main__":
+
+    # parse command line argument of the file name
+    parser = argparse.ArgumentParser()
+    file_name = "file_name"
+    parser.add_argument(file_name, help="The file you want to clean without extension")
+    args = parser.parse_args()
+    config = vars(args)
+
+    df = pd.read_excel(f"{config[file_name]}.xlsx")
+    clean_dataframe(df, config[file_name])
---
+++ dash_app.py
@@ -0,0 +1,59 @@
+from dash import Dash, html, dcc, Input, Output, callback
+from cleaner import clean_dataframe
+import io
+import pandas as pd
+import base64
+from flask import send_file
+
+app = Dash(__name__)
+
+app.layout = html.Div(
+    [
+        html.H1(
+            "Do you want to remove people who haven't filled in their availability on Play Cricket?"
+        ),
+        dcc.Upload(
+            html.Div(
+                [html.A("click here to upload xlsx file")],
+                style={
+                    "width": "50%",
+                    "height": "60px",
+                    "lineHeight": "60px",
+                    "borderWidth": "1px",
+                    "borderStyle": "dashed",
+                    "borderRadius": "5px",
+                    "textAlign": "center",
+                    "margin": "10px",
+                },
+            ),
+            id="file-upload"
+        ),
+        dcc.Download(id="file-download")
+    ]
+)
+
+@callback(
+    Output("file-download", "data"),
+    Input("file-upload", "filename"),
+    Input("file-upload", "contents"),
+    prevent_initial_call=True,
+)
+def upload_and_download_file(file_name, file_contents):
+    if file_contents is not None:
+        content_type, content_string = file_contents.split(',')
+        decoded_content = base64.b64decode(content_string)
+        
+        df = pd.read_excel(io.BytesIO(decoded_content))
+        cleaned_df = clean_dataframe(df, file_name)
+
+        output_file_name = f"cleaned_{file_name}"
+        cleaned_df.to_excel(output_file_name, index=False)
+
+        return dcc.send_file(
+            output_file_name
+        )
+    return None
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
